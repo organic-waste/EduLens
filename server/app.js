4c67fe96829af523aacad8642a592d3547d4de41
organic-waste <15928448056@qq.com>
2025-11-08 18:04:16 +0800

    部署后端到 Render ，部署数据库到MongoDB Atlas

--- server/app.js
+++ server/app.js
@@ -16,10 +16,7 @@ const server = http.createServer(app);
 const wsServer = new WebsocketServer(server);
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB连接成功"))
   .catch((error) => console.log("MongoDB连接失败:", error));
 
